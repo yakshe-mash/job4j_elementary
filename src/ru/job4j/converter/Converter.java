39f3cbfcc9d9efb145c74ccad13f83ea351ce521
yakshe-mash <galardolp550@gmail.com>
2021-01-02 22:04:45 +0300

    5. Конвертер валюты [#440218]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,20 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
+        return rsl;
+    }
+
+    public static int rubleToDollar(int value) {
+        int rsl = value / 60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        int dollar = Converter.rubleToDollar(120);
+        System.out.println("120 rubles are " + dollar + "dollar.");
+    }
+}
