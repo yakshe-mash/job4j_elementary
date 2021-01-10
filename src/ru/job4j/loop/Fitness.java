74e420e933c4818a8c7515fa99465e8221a35c47
yakshe-mash <galardolp550@gmail.com>
2021-01-10 13:38:53 +0300

    5.4. Протеиновая диета [#440276]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while(ivan<nik){
+            ivan*=3;
+            nik*=2;
+            month+=1;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        int result = Fitness.calc(10, 100);
+        System.out.println(result);
+    }
+}
