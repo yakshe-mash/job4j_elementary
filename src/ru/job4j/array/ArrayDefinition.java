a0d26990de5e883f479191cc196ab8ad864254a7
yakshe-mash <galardolp550@gmail.com>
2021-01-12 20:11:07 +0300

    6.0.1. Объявление массива. [#440215]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,6 +4,6 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
-        float[] prices = new float[40];
+        float[] prices = new float[41];
     }
 }
